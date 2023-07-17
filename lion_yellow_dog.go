6b4e694efcc57df634b9f9abf477d5955960ee21
karlantony <136643658+karlantony@users.noreply.github.com>
2023-07-17 08:02:35 +0700

    Update error handling messages

---
+++ lion_yellow_dog.go
@@ -0,0 +1,36 @@
+package main 
+
+import "fmt"
+
+func main() {
+	// A Breath of Fresh Air
+	fmt.Println("Ah, the sweet smell of the morning air")
+	fmt.Println("The sound of birds singing through the trees")
+	fmt.Println("The sight of the sun creeping up the horizon")
+	fmt.Println("It's a breath of fresh air, I can feel it there")
+	fmt.Println("")
+	fmt.Println("I can breathe it in, I can sense it")
+	fmt.Println("Feel the chill of morning in my bones")
+	fmt.Println("There's a calmness that blankets the world")
+	fmt.Println("It's a breath of fresh air, I can feel it now")
+	fmt.Println("")
+	fmt.Println("It's like the first time I ever opened my eyes")
+	fmt.Println("The smell of the grass and the dew on the ground")
+	fmt.Println("The warmth of the sun on my face")
+	fmt.Println("It's a breath of fresh air, I can feel it so clear")
+	fmt.Println("")
+	fmt.Println("As I spin round in circles, I can hear")
+	fmt.Println("The sound of the rushing winds around me")
+	fmt.Println("I look up at the open blue sky")
+	fmt.Println("It's a breath of fresh air, I can feel it in the breeze")
+	fmt.Println("")
+	fmt.Println("I stand there in awe, so overwhelmed")
+	fmt.Println("By the beauty that surrounds me")
+	fmt.Println("The air is so clean and the colors so bright")
+	fmt.Println("It's a breath of fresh air, I can feel it so right")
+	fmt.Println("")
+	fmt.Println("Ah, the sweet smell of the morning air")
+	fmt.Println("The sound of birds singing through the trees")
+	fmt.Println("The sight of the sun creeping up the horizon")
+	fmt.Println("It's a breath of fresh air, I can feel it there")
+}
